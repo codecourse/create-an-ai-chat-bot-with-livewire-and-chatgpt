0b5581d3c2308a8f25cdd1771793935cc4390868
Alex Garrett-Smith <alex@codecourse.com>
2024-05-08 08:52:13 +0100

    Episode 02

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->singleton('openai', function () {
+            return \OpenAI::client(config('openai.secret'));
+        });
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    //
+    $response = app('openai')->chat()->create([
+        'model' => 'gpt-4',
+        'messages' => [
+            ['role' => 'system', 'content' => 'You are a friendly bot to help with web development.'],
+            ['role' => 'user', 'content' => 'Link me to the Laravel docs'],
+        ]
+    ]);
+
+    dd($response);
 });
